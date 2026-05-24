25eafc3c3ffb3a1a7b5ce08eb9984d841a5b6ae1
huweihuang <huweihuang@foxmail.com>
2026-05-24 16:03:45 +0800

    dry-run

--- SUMMARY.md
+++ SUMMARY.md
@@ -1,24 +1,20 @@
-# Java 学习笔记
+# Summary
 
-本仓库是 [胡伟煌的博客](https://www.huweihuang.com) 上的 Java 学习笔记，使用 GitBook 组织与发布。
+## 前言
 
-## 内容结构
+* [目录](README.md)
 
-### 基础
+## 基础
 
 * [数据类型和变量](basic/java-var-type.md)
 * [数组和集合](basic/java-collections.md)
 * [流程控制语句](basic/java-flow-control.md)
 
-### 面向对象编程
+## 面向对象编程
 
 * [类和实例](oop/java-class.md)
 * [Java 接口](oop/java-interface.md)
 
-### 包与构建
+## 包与构建
 
 * [Maven 使用](package/java-maven-usage.md)
-
-## 在线阅读
-
-https://www.huweihuang.com/java-notes/
--- deploy.sh
+++ deploy.sh
@@ -1,27 +1,43 @@
 #!/bin/bash
-# run in MASTER_DIR
 set -x
 set -e
 
 MESSAGE=$1
+DEPLOY_MODE=$2
 
-rm -fr gh-pages
-git clone -b gh-pages https://github.com/huweihuang/java-notes.git gh-pages
+# update blog in blog-source repo
+NOTE_NAME="java-notes"
+BLOG_SOURCE_DIR="../blog-source/${NOTE_NAME}"
 MASTER_DIR="$(pwd)"
 GHPAGE_DIR="${MASTER_DIR}/gh-pages"
+
+# clean and copy blog
+rm -fr $(ls | grep -v -E 'node_modules|book.json|README.md|SUMMARY.md|.gitignore|LICENSE|gh-pages|deploy.sh')
+cp -fr ${BLOG_SOURCE_DIR}/* ./
+
+# clean GHPAGE_DIR
+if [ ! -d $GHPAGE_DIR  ];then
+    git clone -b gh-pages https://github.com/huweihuang/${NOTE_NAME}.git gh-pages
+fi
+rm -fr ${GHPAGE_DIR}/*
 
 # build
 gitbook build
 
-# clean GHPAGE_DIR
-rm -fr ${GHPAGE_DIR}/*
-
 # copy _book to GHPAGE_DIR
 cp -fr ${MASTER_DIR}/_book/* ${GHPAGE_DIR}
 cp -fr ${MASTER_DIR}/README.md ${GHPAGE_DIR}
+cp -fr ${MASTER_DIR}/SUMMARY.md ${GHPAGE_DIR}
 
-# git commit
-cd ${GHPAGE_DIR}
-git add --all
-git commit -m "${MESSAGE}"
-git push origin gh-pages
+if [ $2 = "dry-run" ];then
+    echo "gitbook server dry run"
+    gitbook serve
+else
+    # git commit
+    cd ${GHPAGE_DIR}
+    git add --all
+    git config user.name huweihuang
+    git config user.email huweihuang@foxmail.com
+    git commit -m "${MESSAGE}"
+    git push origin gh-pages
+fi
